a86caa88aa993e92df45764a631d865393efa594
shadowforce78 <planque.adam@gmail.com>
2025-05-19 16:50:45 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module sae.velizy.javasae {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens sae.velizy.javasae to javafx.fxml;
+    exports sae.velizy.javasae;
+}
