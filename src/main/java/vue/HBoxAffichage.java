11b455fd4fdf5eda588b8a503b5ca013aed0cf45
AthouNourane <nourane.athoumani@gmail.com>
2025-05-24 17:04:05 +0200

    Augmentation de l'espace entre les modules

--- src/main/java/vue/HBoxAffichage.java
+++ src/main/java/vue/HBoxAffichage.java
@@ -4,7 +4,7 @@ import javafx.scene.layout.HBox;
 
 public class HBoxAffichage extends HBox {
     public HBoxAffichage(){
-        super(10);
+        super(30);
         VBoxStatCreation panesStatsCreation = new VBoxStatCreation();
         VBoxAffichageParcours tableParcours = new VBoxAffichageParcours();
 
