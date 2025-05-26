60c9c190c478edcbeecff78488f3d92b896de6b4
AthouNourane <nourane.athoumani@gmail.com>
2025-05-26 13:03:02 +0200

    Ajout d'items (scénarios) au menu

--- src/main/java/vue/VBoxRoot.java
+++ src/main/java/vue/VBoxRoot.java
@@ -1,15 +1,22 @@
 package vue;
 
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import modele.ConstantesVues;
 
 public class VBoxRoot extends VBox {
     public VBoxRoot() {
         super(10);
         MenuBar menuBar = new MenuBar();
         Menu menuScenario = new Menu("Scenario");
+        ToggleGroup groupeScenario = new ToggleGroup();
+        for (String item : new ConstantesVues().getItemsMenuScenarios()){
+            RadioMenuItem menuItem = new RadioMenuItem(item);
+            menuItem.setUserData(item);
+            menuScenario.getItems().add(menuItem);
+            menuItem.setToggleGroup(groupeScenario);
+        }
         Menu menuQuitter = new Menu("Quitter");
         menuBar.getMenus().addAll(menuScenario, menuQuitter);
 
