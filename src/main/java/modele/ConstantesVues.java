f91753b82bb3d34187aa909c321ef9355058a467
AthouNourane <nourane.athoumani@gmail.com>
2025-05-26 13:02:21 +0200

    Ajout ConstantesVues.java : création d'une constante pour les scenarios

---
+++ src/main/java/modele/ConstantesVues.java
@@ -0,0 +1,23 @@
+package modele;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ConstantesVues {
+    private static final List<String> ITEMS_MENU_SCENARIO = importScenarios();
+
+    public static List<String> importScenarios(){
+        List<String> scenarios = new ArrayList<>();
+
+        for(File fichierScenario : Objects.requireNonNull(new File("pokemon_appli_data").listFiles())){
+            scenarios.add(fichierScenario.getName());
+        }
+        return scenarios;
+    }
+
+    public List<String> getItemsMenuScenarios(){
+        return ITEMS_MENU_SCENARIO;
+    }
+}
