3c7556bac392a67a1df6870d4b664e43e626d725
AthouNourane <nourane.athoumani@gmail.com>
2025-05-27 17:01:43 +0200

    Amélioration du controleur

--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -3,6 +3,8 @@ package controleur;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.RadioMenuItem;
 import javafx.scene.layout.HBox;
 import vue.*;
 
@@ -12,7 +14,7 @@ public class Controleur implements EventHandler {
     public void handle(Event event) {
         VBoxGauche vBoxGauche = HBoxAffichage.getvBoxGauche();
         GridPaneModification modification = VBoxGauche.getGridModif();
-
+        GridPaneStatistique statistique = VBoxGauche.getGridStatParcours();
         if(event.getSource() instanceof Button){
             if (((Button) event.getSource()).getUserData().equals("Modification")){
                 System.out.println("Modification");
@@ -20,6 +22,15 @@ public class Controleur implements EventHandler {
             else if (((Button) event.getSource()).getUserData().equals("Création")){
                 System.out.println("Création");
             }
+
+            else if(((Button) event.getSource()).getUserData().equals("Stats")){
+                System.out.println("Stats");
+            }
+        }
+
+        if(event.getSource() instanceof RadioMenuItem nouveauScenario){
+            String stringScenario = nouveauScenario.getText();
+            vBoxGauche.updateScenario(stringScenario);
         }
     }
 }
