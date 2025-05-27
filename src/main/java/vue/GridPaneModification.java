0dd7404500020d4072fedf7ca9b2c2a02b988210
AthouNourane <nourane.athoumani@gmail.com>
2025-05-28 00:10:37 +0200

    Changements tailles

--- src/main/java/vue/GridPaneModification.java
+++ src/main/java/vue/GridPaneModification.java
@@ -21,26 +21,17 @@ public class GridPaneModification extends GridPane {
         textClient.setPromptText("Cliquez sur une ligne du tableau...");
         labelClient.setLabelFor(textVendeur);
 
-        Label labelKilometre = new Label("Nombre de kilomètres");
-        TextField textKilometre =  new TextField();
-        textKilometre.setEditable(false);
-        textKilometre.setPromptText("Cliquez sur une ligne du tableau...");
-        labelKilometre.setLabelFor(textKilometre);
-
         Button boutonModification =  new Button("Modifier la ligne");
         boutonModification.getStyleClass().add("button-green");
         boutonModification.setUserData("Modification");
         boutonModification.addEventHandler(ActionEvent.ACTION, controleur);
-
 
         this.add(new Label("Modification d'un Scénario"), 0, 0, 4, 1);
         this.add(labelVendeur, 0, 1);
         this.add(textVendeur, 1, 1, 4, 1);
         this.add(labelClient, 0, 2);
         this.add(textClient, 1, 2, 4, 1);
-        this.add(labelKilometre, 0, 3);
-        this.add(textKilometre, 1, 3, 4, 1);
-        this.add(boutonModification, 1, 4, 2, 1);
+        this.add(boutonModification, 1, 3, 2, 1);
 
     }
 }
