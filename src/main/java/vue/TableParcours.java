d3687a90bef9fe9f20df594478c6aa9d38cdb077
AthouNourane <nourane.athoumani@gmail.com>
2025-05-28 00:07:58 +0200

    Méthode ajoutTable() permettant d'ajouter les parcours dans un tableau

--- src/main/java/vue/TableParcours.java
+++ src/main/java/vue/TableParcours.java
@@ -4,23 +4,39 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
-import modele.Membre;
 import modele.Vente;
+import modele.Ville;
+
+import java.util.List;
 
 public class TableParcours extends VBox {
-    private TableView<Vente> tableParcours;
+    private static TableView<Vente> tableParcours;
     public TableParcours(){
 
         tableParcours = new TableView<>();
-        TableColumn<Vente, Membre> vendeurColumn = new TableColumn<>("Vendeur");
-        TableColumn<Vente, Membre> acheteurColumn = new TableColumn<>("Acheteur");
+        TableColumn<Vente, String> vendeurColumn = new TableColumn<>("Vendeur");
+        TableColumn<Vente, String> acheteurColumn = new TableColumn<>("Acheteur");
 
-        vendeurColumn.setCellValueFactory(new PropertyValueFactory<>("vendeur"));
-        acheteurColumn.setCellValueFactory(new PropertyValueFactory<>("acheteur"));
+        vendeurColumn.setCellValueFactory(new PropertyValueFactory<>("villeVendeur"));
+        acheteurColumn.setCellValueFactory(new PropertyValueFactory<>("villeAcheteur"));
 
         tableParcours.getColumns().add(vendeurColumn);
         tableParcours.getColumns().add(acheteurColumn);
 
+        tableParcours.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+
+
         this.getChildren().add(tableParcours);
+    }
+
+    public static TableView<Vente> getTableParcours() {
+        return tableParcours;
+    }
+
+    public void ajoutTable(List<Ville> itineraire){
+        tableParcours.getItems().clear();
+        for (int i = 0; i < itineraire.size() - 1; i++) {
+            tableParcours.getItems().add(new Vente(itineraire.get(i).getNom(), itineraire.get(i + 1).getNom()));
+        }
     }
 }
