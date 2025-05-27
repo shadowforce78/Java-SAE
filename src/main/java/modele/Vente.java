d787235f6edfef10098c767dd2a6119051ced327
AthouNourane <nourane.athoumani@gmail.com>
2025-05-28 00:04:33 +0200

    Ajout d'un nouveau constructeur et des getters appropriés

--- src/main/java/modele/Vente.java
+++ src/main/java/modele/Vente.java
@@ -3,10 +3,17 @@ package modele;
 public class Vente {
     private Membre vendeur;
     private Membre acheteur;
+    private String villeVendeur;
+    private String villeAcheteur;
 
     public Vente(Membre vendeur, Membre acheteur) {
         this.vendeur = vendeur;
         this.acheteur = acheteur;
+    }
+
+    public Vente(String villeVendeur, String villeAcheteur){
+        this.villeVendeur = villeVendeur;
+        this.villeAcheteur = villeAcheteur;
     }
 
     public Membre getVendeur() {
@@ -18,11 +25,17 @@ public class Vente {
     public Membre getAcheteur() {
         return acheteur;
     }
-
     public void setAcheteur(Membre acheteur) {
         this.acheteur = acheteur;
     }
 
+    public String getVilleVendeur(){
+        return villeVendeur;
+    }
+
+    public String getVilleAcheteur(){
+        return villeAcheteur;
+    }
     @Override
     public String toString() {
         return vendeur.getPseudo() + " → " + acheteur.getPseudo();
