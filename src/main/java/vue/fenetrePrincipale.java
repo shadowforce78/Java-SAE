343c3f3502431b49cbc3c650d04fbea586cb5daa
AthouNourane <nourane.athoumani@gmail.com>
2025-06-04 00:03:07 +0200

    Ajout d'un logo et d'un titre à la fenêtre

--- src/main/java/vue/fenetrePrincipale.java
+++ src/main/java/vue/fenetrePrincipale.java
@@ -2,10 +2,12 @@ package vue;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.Objects;
 
 public class fenetrePrincipale extends Application {
 
@@ -17,6 +19,9 @@ public class fenetrePrincipale extends Application {
     public void start(Stage stage) {
         VBox root = new VBoxRoot();
         Scene scene = new Scene(root, 1200, 700);
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/logo/pokeball_logo.png")));
+        stage.getIcons().add(icon);
+        stage.setTitle("APPLI");
         stage.setScene(scene);
         stage.show();
         stage.setMinHeight(650);
