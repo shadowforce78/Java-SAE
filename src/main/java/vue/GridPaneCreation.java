bc8a685e69baff51fc7ad025bb025555a28bd853
AthouNourane <nourane.athoumani@gmail.com>
2025-06-03 20:00:45 +0200

    Déplacement bouton création

--- src/main/java/vue/GridPaneCreation.java
+++ src/main/java/vue/GridPaneCreation.java
@@ -28,7 +28,7 @@ public class GridPaneCreation extends GridPane {
         this.add(labelTitre, 1, 0, 3, 1);
         this.add(labelNom, 0, 1);
         this.add(textNom, 1, 1, 4, 1);
-        this.add(boutonCreation,2, 2, 2, 1);
+        this.add(boutonCreation,1, 2, 3, 1);
     }
 
     public TextField getTextNom(){
